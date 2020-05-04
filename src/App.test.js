cca7ac7482c0df3d9411e0d1cf728e8d5b1324f4
aurimas-darguzis <aurimas@darguzis.com>
2020-05-04 23:49:02 +0100

    added jest eslint plugin

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { render } from '@testing-library/react';
-// eslint-disable-next-line no-unused-vars
 import App from './App';
 
-// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  // eslint-disable-next-line no-undef
   expect(linkElement).toBeInTheDocument();
 });
